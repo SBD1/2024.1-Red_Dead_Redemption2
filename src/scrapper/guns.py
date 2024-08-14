fa8708fde4994b4e7dbc72ba405cc80293100c88
gitbmvb <bruno.martval@gmail.com>
2024-08-14 18:27:29 -0300

    Personalizando scrapper de armas de fogo

--- src/scrapper/guns.py
+++ src/scrapper/guns.py
@@ -5,13 +5,32 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver1 = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://www.gtabase.com/red-dead-redemption-2/weapons/#sort=name&sortdir=asc")    
+driver.get("https://www.gtabase.com/red-dead-redemption-2/weapons/#sort=name&sortdir=asc&attr.ct55.value=pistols%2Crevolvers%2Cshotguns%2Csniper-rifles%2Crifles%2Crepeaters&page=1")    
 sleep(3)
 guns = []
-guns_find = driver.find_elements(By.CLASS_NAME, "product-item-info ")
-for g in guns_find: guns.append(g.text.strip())
-with open("guns.txt", "w") as f:
-    for g in guns:
-        f.write(g + '\n')
+elements = driver.find_elements(By.CLASS_NAME, "product-item-link")
+for e in elements:
+    href = e.get_attribute("href")
+    print("Accessing", href)
+    driver1.get(href)
+    sleep(3)
+    gun = {
+        "name": driver1.find_element(By.CLASS_NAME, "page-header").text,
+        "description": "".join([p.text for p in driver1.find_elements(By.TAG_NAME, "p")]),
+        "statistics": {item[0]:item[1] for item in [stat.text.split("\n") for stat in driver1.find_elements(By.XPATH, "//*[starts-with(@class, 'field-entry')]")][:9]}
+    }
+    with open("guns_new.txt", "a") as f:
+        name, description = gun["name"], gun["description"]
+        f.write(f"('{name}', '{description}', ")
+        for key in ['DAMAGE', 'FIRE RATE', 'ACCURACY', 'RANGE','RELOAD', 'OVERALL', 'AMMO CAPACITY', 'MANUFACTURER']:
+            try:
+                value = gun['statistics'][key]
+                f.write(f"'{value}', ")
+            except KeyError: f.write(f"'0', ")
+        f.write(")\n")
+    print(f"{name} done. ")
+
+driver1.quit()
 driver.quit()
